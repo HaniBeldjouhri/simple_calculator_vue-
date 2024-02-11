999bde97ecee8b767cf97bd53ec4cc3ed52f7da1
Hani <hani.beldjouhri@gmail.com>
2024-02-11 18:10:17 +0100

    change

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,91 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div id="app">
+    <div class="title span4">{{ displayText }}</div>
+    <div class="grid">
+      <button v-for="char in every" :key="char" @click="handleClick(char)" :class="{ 'span2': char === '=' || char === 'Ac' }">{{ char }}</button>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      displayText: '',
+      lastCharIsOperator: false,
+      operators: ['+', '-', '*', '/', '.'],
+      every: ['Ac', 'Del', '/', '1', '2', '3', '*', '4', '5', '6', '+', '7', '8', '9', '-', '.', '0', '=']
+    };
+  },
+  methods: {
+    handleClick(char) {
+      if (char === '=') {
+        this.calculate();
+      } else if (char === 'Ac') {
+        this.clearDisplay();
+      } else if (char === 'Del') {
+        this.deleteLastChar();
+      } else {
+        this.appendText(char);
+      }
+    },
+    appendText(char) {
+      this.displayText += char;
+      this.lastCharIsOperator = this.operators.includes(char);
+    },
+    clearDisplay() {
+      this.displayText = '';
+      this.lastCharIsOperator = false;
+    },
+    deleteLastChar() {
+  let textArr = this.displayText.toString().split('');
+  textArr.pop();
+  this.displayText = textArr.join('');
+  this.lastCharIsOperator = this.operators.includes(textArr[textArr.length - 1]);
+},
+    calculate() {
+      if (!!this.displayText) {
+        this.displayText = eval(this.displayText);
+      }
+    }
+  }
+};
+</script>
+
+<style>
+       .title{
+            height: 100px;
+            padding: 5px;
+            border-radius:5px ;
+            margin: 5px;
+            color: white;
+            background-color: rgb(43, 42, 42);
+            font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+            overflow: hidden;
+            font-size: xx-large;
+        }
+        .grid{
+            display: grid;
+            grid-template-columns: auto auto auto auto;
+        }
+        button{
+            padding: 30px;
+            margin: 5px;
+            border: 0;
+            color: white;
+            background-color: grey;
+            border-radius:5px ;
+            font-size: 20px;
+            cursor: pointer;
+        }
+        .span2{
+            grid-column: span 2;
+        }
+        .span4{
+            grid-column: span 4;
+        }
+        button:hover{
+            background-color: darkgrey;
+        }
 </style>
